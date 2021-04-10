315309e38745de39a18c77ce67a7e6ec0660b432
Benoit KUGLER <x.ben.x@free.fr>
2021-04-10 19:23:40 +0200

    simple automation to track HarfBuzz reference implementation

---
+++ harfbuzz/track_reference/main.go
@@ -0,0 +1,156 @@
+// Script aiming at checking the progress of the reference implementation,
+// using Git.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var (
+	referenceGitDirectory = "/home/benoit/Téléchargements/harfbuzz"
+	originCommitID        = "e19de65eaeca135b2c735439185e35bfe1c0aeb7"
+)
+
+const remoteReferenceURL = "https://github.com/harfbuzz/harfbuzz"
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// parse the command line
+func setupOptions() {
+	refGit := flag.String("ref", "", "Git directory of the HarfBuzz reference implementation")
+	origin := flag.String("origin", originCommitID, "CurrentCommit ID of the port")
+	flag.Parse()
+
+	if *refGit == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	referenceGitDirectory = *refGit
+	originCommitID = *origin
+}
+
+func getCommitsSince(originCommitID string) ([]string, error) {
+	cmd := exec.Command("git", "rev-list", "--reverse", fmt.Sprintf("%s..HEAD", originCommitID))
+	cmd.Dir = referenceGitDirectory
+	commits, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(strings.TrimSpace(string(commits)), "\n"), nil
+}
+
+func getFilesFromCommit(commitID string) ([]string, error) {
+	cmd := exec.Command("git", "diff-tree", "-r", "--name-only", "--no-commit-id", commitID)
+	cmd.Dir = referenceGitDirectory
+	files, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(strings.TrimSpace(string(files)), "\n"), nil
+}
+
+var regexpKind = regexp.MustCompile(`\[(\w+)\]`)
+
+// return the topic of the commit or an empty string if not found
+func getCommitKind(commitID string) (string, error) {
+	cmd := exec.Command("git", "log", "-n 1", "--format=%s", commitID)
+	cmd.Dir = referenceGitDirectory
+	subject, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	if m := regexpKind.FindSubmatch(subject); m != nil {
+		return string(m[1]), nil
+	}
+	return "", nil
+}
+
+type index []string
+
+func (i index) isTrackingFile(file string) bool {
+	for _, trackFileOrDir := range i {
+		if strings.HasPrefix(file, trackFileOrDir) {
+			return true
+		}
+	}
+	return false
+}
+
+// parse the reference index
+func getTrackedFiles() (index, error) {
+	b, err := ioutil.ReadFile("references_files.txt")
+	if err != nil {
+		return nil, err
+	}
+	filesOrDirs := strings.Split(string(b), "\n")
+	return filesOrDirs, nil
+}
+
+type commitFiles struct {
+	commitID     string
+	trackedFiles []string // included in the reference index
+	ignoredFiles []string // other files
+}
+
+func listChangedFiles() []commitFiles {
+	trackedFiles, err := getTrackedFiles()
+	check(err)
+
+	commits, err := getCommitsSince(originCommitID)
+	check(err)
+
+	var out []commitFiles
+	for _, commit := range commits {
+		files, err := getFilesFromCommit(commit)
+		check(err)
+
+		cf := commitFiles{commitID: commit}
+		for _, file := range files {
+			if trackedFiles.isTrackingFile(file) {
+				cf.trackedFiles = append(cf.trackedFiles, file)
+			} else {
+				cf.ignoredFiles = append(cf.ignoredFiles, file)
+			}
+		}
+
+		kind, err := getCommitKind(commit)
+		check(err)
+
+		// we simply ignore the commits with no tracked files
+		// we also ignore the commits concerning the subset feature
+		if len(cf.trackedFiles) == 0 || kind == "subset" {
+			continue
+		}
+		out = append(out, cf)
+	}
+
+	return out
+}
+
+func main() {
+	setupOptions()
+
+	changes := listChangedFiles()
+
+	for _, change := range changes {
+		url := remoteReferenceURL + "/commit/" + change.commitID
+		fmt.Printf("commit %s : (%d) files ignored\n", url, len(change.ignoredFiles))
+		for _, file := range change.trackedFiles {
+			fmt.Println("\t", file)
+		}
+	}
+}
---
+++ harfbuzz/track_reference/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetCommits(t *testing.T) {
+	originCommitID := "fd489433a8a223068d904e04d4ce8bafa82900c7"
+	commits, err := getCommitsSince(originCommitID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(commits)
+}
+
+func TestGetFiles(t *testing.T) {
+	commitID := "fd489433a8a223068d904e04d4ce8bafa82900c7"
+	files, err := getFilesFromCommit(commitID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(files)
+}
+
+func TestGetKind(t *testing.T) {
+	commitID := "fd489433a8a223068d904e04d4ce8bafa82900c7"
+	kind, err := getCommitKind(commitID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(kind)
+}
+
+func TestParseTrackedFiles(t *testing.T) {
+	filesOrDirs, err := getTrackedFiles()
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(filesOrDirs)
+}
